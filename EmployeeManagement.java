df058c436af995f9cfbb1069f3ba68d97119f5fa
shivamsaini006 <69237471+shivamsaini006@users.noreply.github.com>
2020-08-05 14:49:45 +0530

    Added Difference Module

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -1,5 +1,8 @@
 class calculator{
   public void add(int a, int b){
-    System.out.println("Sun is "+(a+b));
+    System.out.println("Sum is "+(a+b));
+  }
+   public void diff(int a, int b){
+    System.out.println("Difference is "+(a-b));
   }
 }
